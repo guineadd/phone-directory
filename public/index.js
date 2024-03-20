63147705fe75a4efbd49b80eaedc9cf6605017be
dkiprianou <dkiprianou@olympia-electronics.gr>
2024-03-20 14:35:58 +0200

    user management implementation

--- public/index.js
+++ public/index.js
@@ -1,11 +1,14 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./styles/styles.css";
 import Login from "./components/login/login.js";
-import loginTemplate from "./components/login/login.html";
 import Catalog from "./components/catalog/catalog.js";
+import Notifications from "./components/notifications/notifications.js";
+import UserManagement from "./components/user-management/user-management.js";
+
+import loginTemplate from "./components/login/login.html";
 import catalogTemplate from "./components/catalog/catalog.html";
 import notificationsTemplate from "./components/notifications/notifications.html";
-import Notifications from "./components/notifications/notifications.js";
+import userManagementTemplate from "./components/user-management/user-management.html";
 
 // insert html templates into containers
 const loginElement = document.getElementById("login-container");
@@ -17,11 +20,17 @@ catalogElement.innerHTML = catalogTemplate;
 const notificationsElement = document.getElementById("notification-bar");
 notificationsElement.innerHTML = notificationsTemplate;
 
+const userManagementElement = document.getElementById("user-management-container");
+userManagementElement.innerHTML = userManagementTemplate;
+
 const login = new Login();
 const catalog = new Catalog();
 const notifications = new Notifications();
+const userManagement = new UserManagement();
 
 catalog.render();
 login.render();
+userManagement.render();
 catalog.setComponents(login, notifications);
 login.setComponents(catalog, notifications);
+userManagement.setComponents(login, notifications);
