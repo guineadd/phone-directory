f4d8f8a43e3dabc86ebb2932b9a0f743070ff75c
dkiprianou <dkiprianou@olympia-electronics.gr>
2024-03-20 14:29:31 +0200

    create pdf exportation form

--- public/components/catalog/catalog.js
+++ public/components/catalog/catalog.js
@@ -1,13 +1,22 @@
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
+
 export default class Catalog {
   constructor() {
     this.loginBtn = null;
+    this.createPdfBtn = null;
     this.showLoginModal = this.showLoginModal.bind(this);
+    this.createPdf = this.createPdf.bind(this);
   }
 
   render() {
     this.loginBtn = document.getElementById("login-button");
+    this.createPdfBtn = document.getElementById("export-button");
+
     this.removeClickListener(this.loginBtn, this.showLoginModal);
     this.addClickListener(this.loginBtn, this.showLoginModal);
+    this.removeClickListener(this.createPdfBtn, this.createPdf);
+    this.addClickListener(this.createPdfBtn, this.createPdf);
   }
 
   setComponents(notifications) {
@@ -19,6 +28,21 @@ export default class Catalog {
     if (loginModalContainer) {
       loginModalContainer.classList.remove("hidden");
     }
+  }
+
+  createPdf() {
+    const pdf = new jsPDF({
+      unit: "mm",
+      format: "a4",
+      orientation: "portrait",
+    });
+    const pdfContainer = document.getElementById("pdf-container");
+
+    html2canvas(pdfContainer, { scale: 4 }).then(canvas => {
+      const imgData = canvas.toDataURL("image/png");
+      pdf.addImage(imgData, "PNG", 10, 10, 190, 277);
+      pdf.save("filename.pdf");
+    });
   }
 
   addClickListener(element, handler) {
